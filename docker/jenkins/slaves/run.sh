d757a1ec872cc19b25de2a5507a11758c3fed5e8
Joshua Richard <eos.josh.richard@gmail.com>
2015-12-03 15:00:43 -0500

    Adding authentication for jenkins slaves

--- docker/jenkins/slaves/run.sh
+++ docker/jenkins/slaves/run.sh
@@ -6,4 +6,4 @@ echo "Starting jenkins slave \"${SLAVE_NAME}\" with the labels \"${SLAVE_LABELS}
 
 su -p -l \
     -s "/bin/bash" \
-    -c "java -jar /var/tmp/swarm-client.jar -labels \"${SLAVE_LABELS}\" -executors \"4\" -disableSslVerification -name \"${SLAVE_NAME}\" -fsroot \"/var/tmp\""
+    -c "java -jar /var/tmp/swarm-client.jar -labels \"${SLAVE_LABELS}\" -executors \"4\" -disableSslVerification -name \"${SLAVE_NAME}\" -username \"josh\" -password \"xxx\" -fsroot \"/var/tmp\""
