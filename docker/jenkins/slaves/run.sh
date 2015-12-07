ac58cba886f7e44a56e498a9f555ddc6fd9ce791
joshuarichard <eos.josh.richard@gmail.com>
2015-12-07 12:39:50 -0500

    Update run.sh

--- docker/jenkins/slaves/run.sh
+++ docker/jenkins/slaves/run.sh
@@ -2,8 +2,6 @@
 
 SLAVE_NAME="${SLAVE_NAME_PREFIX}-`hostname`"
 
-echo "Starting jenkins slave \"${SLAVE_NAME}\" with the labels \"${SLAVE_LABELS}\""
-
 su -p -l \
     -s "/bin/bash" \
-    -c "java -jar /var/tmp/swarm-client.jar -labels \"${SLAVE_LABELS}\" -executors \"4\" -disableSslVerification -name \"${SLAVE_NAME}\" -username \"josh\" -password \"xxx\" -fsroot \"/var/tmp\""
+    -c "java -jar /var/tmp/swarm-client.jar -labels \"${SLAVE_LABELS}\" -executors \"4\" -disableSslVerification -name \"${SLAVE_NAME}\" -username \"putuserhere\" -password \"putpasswordhere\" -fsroot \"/var/tmp\""
