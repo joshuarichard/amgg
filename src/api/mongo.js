3b7042303adfeccc0626a249e8a0d43e7d4b76d5
Josh Richard <eos.josh.richard@gmail.com>
2015-12-31 00:36:14 -0500

    Adding insertDocuments()

--- src/api/mongo.js
+++ src/api/mongo.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var nconf = require('nconf');
 nconf.env()
-     .file({ file: 'config.json'});
+     .file({ file: 'config.json' });
 
 var dbName = nconf.get('autofill:dbName');
 var url = 'mongodb://' + nconf.get('mongo:host') + ':' +
@@ -10,8 +10,67 @@ var url = 'mongodb://' + nconf.get('mongo:host') + ':' +
 
 var exports = module.exports = {};
 
-exports.insert = function (doc) {
-    return doc;
+/** insertDocuments(collection, docs, callback)
+ *
+ * insert any number of documents into a specific collection.
+ *
+ * collection   (string) - the collection to search for documents
+ * docs (JSON or array)  - docs to insert. one JSON doc or array of JSON docs
+ * callback       (func) - callback function to execute after completion
+ *
+ * example implementation:
+ * -----------------------
+ * var mongoapi = require('./src/api/mongo.js');
+ *
+ * var docs = [{ 'name': 'Poe Dameron' }, { 'name' : 'Kylo Ren' }];
+ * mongoapi.insertDocuments('testing', docs, function(isOk) {
+ *     if(isOk) {
+ *         console.log('everything\'s okay');
+ *     } else {
+ *         console.log('everything sucks');
+ *
+ * });
+ *
+ */
+exports.insertDocuments = function(collection, docs, callback) {
+    var isOk = false;
+
+    var insertDoc = function(db, collection, doc, callback) {
+        db.collection(collection).insertOne(doc, function(err, result) {
+            assert.equal(err, null);
+            callback(result);
+        });
+    };
+
+    var bulkInsert = function(db, collection, docs, callback) {
+        var bulk = db.collection(collection).initializeUnorderedBulkOp();
+        for (var i = 0; i < docs.length; i++) {
+            bulk.insert(docs[i]);
+        }
+
+        // execute bulk op and make some assertions
+        bulk.execute(function(err, result) {
+            assert.equal(docs.length, result.nInserted);
+            callback(result);
+        });
+    };
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        if(docs instanceof Array) {
+            bulkInsert(db, collection, docs, function() {
+                db.close();
+                isOk = true;
+                callback(isOk);
+            });
+        } else {
+            insertDoc(db, collection, docs, function() {
+                db.close();
+                isOk = true;
+                callback(isOk);
+            });
+        }
+    });
 };
 
 // lookupKeys - array of keys
