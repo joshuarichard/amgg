19d2917dcf4677346e4fc742c268c50657ad0e00
Darnuss <jacobbuettner@gmail.com>
2016-01-04 15:31:16 -0500

    a few minor changes

--- public/js/children.js
+++ public/js/children.js
@@ -4,31 +4,4 @@ var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://73.227.187.84:27017';
 
 $(document).ready(function () {
-	var loadChildrenPictures = function() {
-		//number of children pictures we need
-		var numChildrenPicture = $(".child-picture").length; 
-
-
-		// //function to return all documents in the children collection
-		// var findChildren = function(db, callback) {
-		//    var cursor = db.collection('children').find( );
-		//    cursor.each(function(err, doc) {
-		//       assert.equal(err, null);
-		//       if (doc != null) {
-		//          console.dir(doc);
-		//       } else {
-		//          callback();
-		//       }
-		//    });
-		// };
-
-		// MongoClient.connect(url, function(err, db) {
-		//   assert.equal(null, err);
-		//   findRestaurants(db, function() {
-		//       db.close();
-		//   });
-		// });
-
-		
-	};
 });
