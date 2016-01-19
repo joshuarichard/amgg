65675978fcf38b29cfbd1796db628068ed5ad865
Darnuss <jacobbuettner@gmail.com>
2016-01-19 14:13:03 -0500

    fixing find-a-child box, wrote js but haven't tested

--- public/js/children.js
+++ public/js/children.js
@@ -4,6 +4,25 @@
 $(document).ready(function () {
     // load all unsponsored kids into a demo array
     // and put one randomly in checkout.html
+
+    function pic(callback) {    
+        // get the picture and load it in
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/pictures/' + id,
+            beforeSend: function (xhr) {
+                xhr.overrideMimeType('text/plain; charset=x-user-defined');
+            },
+            success: function (result, textStatus, jqXHR) {
+                var data = jqXHR.responseText;
+                // picIMG.src = 'data:image/image;base64,' + data;
+                $(".child-picture").attr("src",'data:image/image;base64,' + data);                
+                callback();
+            }
+        });        
+    }
+
+    console.log("hello");
     function data(callback) {
             // get child data using api
         $.getJSON('/api/v1/children/' + id, function(res) {
@@ -26,6 +45,12 @@ $(document).ready(function () {
             var div = document.createElement('td');
             div.className = 'child-info-group';
 
+            $("#child-name").text(name);
+            $("#child-age").text(age);
+            $("#child-gender").text(gender);
+            $("#child-location").text(center);
+            // $(".child-picture").attr("src","../image/nino-1.png");
+
             var nameDiv = document.createElement('div');
             var ageDiv = document.createElement('div');
             var birthdayDiv = document.createElement('div');
@@ -46,19 +71,12 @@ $(document).ready(function () {
             genderDiv.innerHTML = 'Género: ' + gender;
             centerDiv.innerHTML = 'Centro de Ninos: ' + center;
 
-            // append children to div
-            td.appendChild(nameDiv);
-            td.appendChild(ageDiv);
-            td.appendChild(birthdayDiv);
-            td.appendChild(genderDiv);
-            td.appendChild(centerDiv);
+            //load values onto page
 
-            div.appendChild(td);
-            tr.appendChild(div);
 
             callback();
         });
-    }
+    }    
 
     $('.child-picture').click(function() {
         localStorage['children'] = ids.toString();
