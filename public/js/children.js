a738d21f3de4f1701462f0faa7b56118eb801cb6
Darnuss <jacobbuettner@gmail.com>
2016-01-21 16:55:36 -0500

    loading child info, still working on getting the pics to load in

--- public/js/children.js
+++ public/js/children.js
@@ -3,6 +3,7 @@
 
 $(document).ready(function () {
     var id = '';
+    
     function pic() {
         // get the picture and load it in
         $.ajax({
@@ -21,41 +22,49 @@ $(document).ready(function () {
     function data(callback) {
         // get all unsponsored kids and pick one to display in the carousel
         $.getJSON('/api/v1/unsponsored', function(res) {
-            // calculate the resLength for random child bounds
-            var key, resLength = 0;
-            for(key in res) {
-                if(res.hasOwnProperty(key)) {
-                    resLength++;
+
+            //loop through all the slides
+            for (x=1; x <= $(".child-slide").length; x++) {
+
+                // calculate the resLength for random child bounds
+                var key, resLength = 0;
+                for(key in res) {
+                    if(res.hasOwnProperty(key)) {
+                        resLength++;
+                    }
                 }
+
+                // use the resLength to randomly pick one of the unsponsored
+                // children within the bounds
+                var ran = Math.floor(Math.random() * (resLength - 1) + 1);
+
+                // now iterate over the res with an index (i) and match it to the
+                // random number.
+                var i = 0;
+                for (key in res) {
+                    // if index === random number then pick this child
+                    if (i === ran && res.hasOwnProperty(key)) {
+                        id = key;
+                        console.log('child-name' + x);
+                        document.getElementById('child-name' + x).innerHTML = res[id].nombre;
+                        console.log('child-name' + x);
+                        document.getElementById('child-age' + x).innerHTML = res[id].años;
+                        console.log('child-age' + x);
+                        document.getElementById('child-gender' + x).innerHTML = res[id].género;
+                        console.log('child-gender' + x);
+                        document.getElementById('child-location' + x).innerHTML = res[id].centro_de_ninos;
+                        console.log('child-location' + x);
+                        break;
+                    } else {
+                        i++;
+                    }
+                }
+                callback();
             }
 
-            // use the resLength to randomly pick one of the unsponsored
-            // children within the bounds
-            var ran = Math.floor(Math.random() * (resLength - 1) + 1);
-
-            // now iterate over the res with an index (i) and match it to the
-            // random number.
-            var i = 0;
-            for (key in res) {
-                // if index === random number then pick this child
-                if (i === ran && res.hasOwnProperty(key)) {
-                    id = key;
-                    document.getElementById('child-name')
-                            .innerHTML = res[id].nombre;
-                    document.getElementById('child-age')
-                            .innerHTML = res[id].años;
-                    document.getElementById('child-gender')
-                            .innerHTML = res[id].género;
-                    document.getElementById('child-location')
-                            .innerHTML = res[id].centro_de_ninos;
-                    break;
-                } else {
-                    i++;
-                }
-            }
-            callback();
         });
-    }
+    }    
+    
 
     function insertChildIntoCarousel() {
         data(function() {
