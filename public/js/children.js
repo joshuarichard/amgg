58b584f5a61670afbd8876fa4808d5ebaf405c0b
Darnuss <jacobbuettner@gmail.com>
2016-03-22 00:51:59 -0400

    checkstyle

--- public/js/children.js
+++ public/js/children.js
@@ -480,62 +480,64 @@ $(document).ready(function() {
         var confirmPassword = $('[name=password-confirm]', form)[0];
 
         console.log(firstName);
-        if(firstName.value == "") {
-            alert("Error: First name cannot be blank!");
+        if(firstName.value == '') {
+            alert('Error: First name cannot be blank!');
             firstName.focus();
             return false;
-        } if(lastName.value == "") {
-            alert("Error: Last name cannot be blank!");
+        } if(lastName.value == '') {
+            alert('Error: Last name cannot be blank!');
             lastName.focus();
             return false;
-        } else if(phone.value == "") {
-            alert("Error: Phone number cannot be blank!");
+        } else if(phone.value == '') {
+            alert('Error: Phone number cannot be blank!');
             phone.focus();
             return false;
-        } else if(street.value == "") {
-            alert("Error: Street address cannot be blank!");
+        } else if(street.value == '') {
+            alert('Error: Street address cannot be blank!');
             street.focus();
             return false;
-        } else if(city.value == "") {
-            alert("Error: City cannot be blank!");
+        } else if(city.value == '') {
+            alert('Error: City cannot be blank!');
             city.focus();
             return false;
-        } else if(email.value == "") {
-            alert("Error: Email cannot be blank!");
+        } else if(email.value == '') {
+            alert('Error: Email cannot be blank!');
             email.focus();
             return false;
         }
-        else if(password.value != "" && password.value == confirmPassword.value) {
+        else if(password.value != '' && password.value == confirmPassword.value) {
             if(password.value.length < 6) {
-                alert("Error: Password must contain at least six characters!");
+                alert('Error: Password must contain at least six characters!');
                 password.focus();
                 return false;
             }
             if(password.value == firstName.value || password.value == lastName.value) {
-                alert("Error: Password must be different from your name!");
+                alert('Error: Password must be different from your name!');
                 password.focus();
                 return false;
             }
             re = /[0-9]/;
             if(!re.test(password.value)) {
-                alert("Error: password must contain at least one number (0-9)!");
+                alert('Error: password must contain at least one number (0-9)!');
                 password.focus();
                 return false;
             }
             re = /[a-z]/;
             if(!re.test(password.value)) {
-                alert("Error: password must contain at least one lowercase letter (a-z)!");
+                alert('Error: password must contain at least one lowercase letter (a-z)!');
                 password.focus();
                 return false;
             }
             re = /[A-Z]/;
             if(!re.test(password.value)) {
-                alert("Error: password must contain at least one uppercase letter (A-Z)!");
+                alert('Error: password must contain at least one uppercase letter (A-Z)!');
                 password.focus();
                 return false;
             }
         } else {
+            /* eslint-disable */
             alert("Error: Please check that you've entered and confirmed your password!");
+            /*eslint-enable */
             password.focus();
             return false;
         }
