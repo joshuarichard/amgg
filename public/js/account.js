30ba9f20a28a22364df6e7970d06ef03e7db7014
Darnuss <jacobbuettner@gmail.com>
2016-04-04 09:53:47 -0400

    redoing style changes

--- public/js/account.js
+++ public/js/account.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
         /* create the content for tabA */
         var tabAHeader = document.createElement('span');
         tabAHeader.className = 'header';
-        tabAHeader.innerHTML = 'niños apadrinados';
+        tabAHeader.innerHTML = 'Niños Apadrinados';
 
         var tabAHeaderhr = document.createElement('hr');
         tabA.appendChild(tabAHeader);
@@ -74,7 +74,7 @@ $(document).ready(function() {
         var letter = document.createElement('textarea');
         letter.className = 'letter';
         var submitLetter = document.createElement('button');
-        submitLetter.className = 'letter-submit pull-right';
+        submitLetter.className = 'btn btn-md btn-primary letter-submit pull-right';
         submitLetter.title = 'Send letter to your sponsored child';
         submitLetter.innerHTML = 'Send';
 
@@ -151,7 +151,7 @@ $(document).ready(function() {
                 //tabB header
                 tabBHeader = document.createElement('span');
                 tabBHeader.className = 'header';
-                tabBHeader.innerHTML = 'datos de facturación';
+                tabBHeader.innerHTML = 'Datos de Facturación';
                 var tabBHeaderhr = document.createElement('hr');
 
                 //create container for user info
@@ -341,7 +341,7 @@ $(document).ready(function() {
                 var submitPasswordChanges = document.createElement('button');
                 submitPasswordChanges.id = 'submit-new-password';
                 submitPasswordChanges.className = 'btn btn-primary btn-sm pull-right';
-                submitPasswordChanges.innerHTML = 'submit password';
+                submitPasswordChanges.innerHTML = 'Submit Password';
                 submitContainer.appendChild(submitPasswordChanges);
 
                 //combine the password elements into one block so we can target them together with css
@@ -368,7 +368,7 @@ $(document).ready(function() {
                 var changePasswordButton = document.createElement('button');
                 changePasswordButton.id = 'change-password-button';
                 changePasswordButton.className = 'btn btn-success btn-sm';
-                changePasswordButton.appendChild(document.createTextNode('change password'));
+                changePasswordButton.appendChild(document.createTextNode('Change Password'));
                 userInfoSidebar.appendChild(changePasswordButton);
 
                 //toggle the password form
@@ -568,15 +568,18 @@ $(document).ready(function() {
         if ($('.contraseña-container').css('display') == 'block') {
             $('.contraseña-container').hide();
         }
+        var editInforContainer = document.createElement('div');
+        editInforContainer.className = 'col-md-11';
         var editInfoSubmit = document.createElement('button');
         editInfoSubmit.id = 'edit-info-submit';
-        editInfoSubmit.className = 'col-md-12 btn btn-primary pull-right';
-        editInfoSubmit.innerHTML = 'enviar';
+        editInfoSubmit.className = 'btn btn-md btn-primary pull-right';
+        editInfoSubmit.innerHTML = 'Enviar';
         editInfoSubmit.title = 'submit changes to your information';
         editInfoSubmit.onclick = function() {
             submitInfoChanges();
         };
-        $('#tabB-content').append(editInfoSubmit);
+        editInforContainer.appendChild(editInfoSubmit)
+        $('.user-info-container').append(editInforContainer);
     }
 
     function infoChange () {
@@ -754,7 +757,7 @@ $(document).ready(function() {
                         // create button, add classname for styling, append text
                         var button = document.createElement('button');
                         button.className = 'btn btn-primary btn-sm';
-                        button.appendChild(document.createTextNode('eliminar'));
+                        button.appendChild(document.createTextNode('Eliminar'));
 
                         // set on click button function
                         button.onclick = function() {
