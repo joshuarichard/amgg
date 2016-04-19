9da5627385422a4ec163b1b178b65d02312a8e75
Darnuss <jacobbuettner@gmail.com>
2016-04-18 22:40:00 -0400

    added cart link to index, children, checkout, and account pages

--- public/js/children.js
+++ public/js/children.js
@@ -99,13 +99,47 @@ $(document).ready(function() {
         }
     }
 
-    // Initialize updateCart function can call it
+    /* Initialize updateCart and then call it
+     * 1. If there is an id in sessionStorage then the user is logged in and
+     *    we need to check the DB for a cart
+     * 2. If not, check sessionStorage for a cart
+     * 3. Otherwise leave the cart at its initialized 0 state
+     */
     function updateCart() {
-        if (inStorage('cart')) {
+        if (inStorage('id')) {
+            $.ajax({
+                url: '/api/v1/donor/cart/id/' + sessionStorage.getItem('id'),
+                type: 'GET',
+                success: function(res) {
+                    if (JSON.stringify(res) !== '{}') {
+                        var kidsInCartInDB = [];
+                        if (inStorage('cart')) {
+                            var kidsInCartOnPage = sessionStorage.getItem('cart').split(',');
+                            for (var key in res) {
+                                kidsInCartInDB = res[key]['kids_in_cart'];
+                                for (var c = 0; c < kidsInCartInDB.length; c++) {
+                                    if (kidsInCartOnPage.indexOf(kidsInCartInDB[c]) === -1) {
+                                        kidsInCartOnPage.push(kidsInCartInDB[c]);
+                                    }
+                                }
+                                sessionStorage.setItem('cart', kidsInCartOnPage.toString());
+                                $('.counter').html(' (' + sessionStorage.getItem('cart').split(',').length + ')');
+                            }
+                        } else {
+                            for (key in res) {
+                                kidsInCartInDB = res[key]['kids_in_cart'];
+                                sessionStorage.setItem('cart', kidsInCartInDB.toString());
+                                $('.counter').html(' (' + sessionStorage.getItem('cart').split(',').length + ')');
+                            }
+                        }
+                    } else if (inStorage('cart')) {
+                        $('.counter').html(' (' + sessionStorage.getItem('cart').split(',').length + ')');
+                    }
+                }
+            });
+        } else if (inStorage('cart')) {
             $('.counter').html(' (' + sessionStorage.getItem('cart').split(',').length + ')'); 
-        } else {
-            $('.counter').html(' (0)');
-        }
+        } 
     }
     updateCart();
     
