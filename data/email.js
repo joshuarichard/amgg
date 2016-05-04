ad67c324364d8379597773f5fbd44ee9ca24f678
Josh Richard <eos.josh.richard@gmail.com>
2016-05-04 13:33:39 -0400

    encrypting email and password for admin

--- data/email.js
+++ data/email.js
@@ -1,22 +1,47 @@
 var nodemailer = require('nodemailer');
 var nconf = require('nconf');
+var argv = require('minimist')(process.argv.slice(2));
+var crypto = require('crypto');
 
 nconf.file({
     file: './config.json'
 });
 
-var adminEmail = nconf.get('admin:email');
-var adminPassword = nconf.get('admin:password');
+var algorithm = 'aes-256-ctr';
+
+// encrypt and decrypt functions taken from:
+// http://lollyrock.com/articles/nodejs-encryption/
+function decrypt(text, pass) {
+    var decipher = crypto.createDecipher(algorithm, pass);
+    var decrypted = decipher.update(text, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+var decryptedEmail = decrypt(nconf.get('admin:email'), argv.password);
+decryptedEmail = decryptedEmail.split('|');
+var emailHash = crypto.createHash('md5')
+                      .update(decryptedEmail[0] + '|' +
+                              decryptedEmail[1])
+                      .digest('hex');
+
+if (emailHash !== decryptedEmail[2]) {
+    log.error('Incorrect password given at startup. Bank worked but email didn\'t.');
+    process.exit();
+}
+
+var ADMIN_EMAIL = decryptedEmail[0];
+var ADMIN_PASSWORD = decryptedEmail[1];
 
 var exports = module.exports = {};
 
 exports.email = function(toAddress, header, body, callback) {
     // create reusable transporter object using the default SMTP transport
-    var transporter = nodemailer.createTransport('smtps://' + adminEmail + ':' + adminPassword + '@smtp.gmail.com');
+    var transporter = nodemailer.createTransport('smtps://' + ADMIN_EMAIL + ':' + ADMIN_PASSWORD + '@smtpout.secureserver.net');
 
     // setup e-mail data with unicode symbols
     var mailOptions = {
-        from: 'AMG Guatemala <' + adminEmail + '>',
+        from: 'AMG Guatemala <' + ADMIN_EMAIL + '>',
         to: toAddress,
         subject: header,
         text: body
@@ -24,8 +49,8 @@ exports.email = function(toAddress, header, body, callback) {
 
     // send mail with defined transport object
     transporter.sendMail(mailOptions, function(error) {
-        if(error){
-            callback(false);
+        if(error) {
+            callback(error);
         } else {
             callback(true);
         }
