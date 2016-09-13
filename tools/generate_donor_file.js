8e6febae2e3b922cb12ec1c76229e06b20b32cd0
Josh Richard <eos.josh.richard@gmail.com>
2016-09-13 18:25:14 -0400

    should be all done with recurring payment utility

--- tools/generate_donor_file.js
+++ tools/generate_donor_file.js
@@ -1,20 +1,44 @@
 var crypto = require('crypto');
 var nconf = require('nconf');
 var mongo = require('../data/mongo.js');
+var csvWriter = require('csv-write-stream');
+var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+
+var writer = csvWriter({sendHeaders: false});
 
 nconf.file({
     file: './config.json'
 });
 
+function decrypt(text) {
+    var decipher = crypto.createDecipher('aes-256-ctr', argv.password)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
+}
+
 mongo.find({'niños_patrocinadoras':{$ne:null}}, nconf.get('mongo:donorCollection'), 10000, false, function(donorDocs) {
-    console.log(JSON.stringify(donorDocs));
+    var donor = {};
+    writer.pipe(fs.createWriteStream('donor_payments_' + new Date() + '.csv'));
 
     for (var i = 0; i < donorDocs.length; i++) {
-        delete donorDocs[i].transacciones;
-        delete donorDocs[i].password;
-        delete donorDocs[i].salt;
+        donor = {};
+
+        var date = new Date();
+        var exp = decrypt(donorDocs[i].expiration);
+
+        donor.one = 1;
+        donor.name = donorDocs[i].nombre + ' ' + donorDocs[i].apellido;
+        donor.ref = 'Padrino ' + (i + 1);
+        donor.ccnumber = decrypt(donorDocs[i].ccnumber);
+        donor.exp = exp.slice(0,2) + '20' + exp.slice(2, 4);
+        donor.amount = 200;
+        donor.date = date.getDate().toString() + (date.getMonth() + 1).toString() + date.getFullYear().toString().replace('20', '');
+        donor.email = donorDocs[i].correo_electrónico;
+        writer.write(donor);
+
     }
 
-    console.log('===================================================');
-    console.log(donorDocs);
+    writer.end();
 });
